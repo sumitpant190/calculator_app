e5809b2efaeed7c34db9f8b3a1c2ac642572cf09
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-04-28 15:34:13 +0545

    1st commit

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,16 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final String title;
+  final Color color;
+  final VoidCallback onPress;
+  const MyButton({
+    Key? key,
+    required this.title,
+    this.color = const Color(0xffa5a5a5),
+    required this.onPress,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.orange),
-      child: Center(
-        child: Text('1'),
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
+        child: InkWell(
+          onTap: onPress,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
